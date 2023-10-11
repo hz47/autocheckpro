3bc83b275221b460c2ec8eb7bc240931ba6595a9
Milos Oroz <milos.oroz@gmail.com>
2023-10-11 21:31:06 +0200

    adapt home page

--- js/common.js
+++ js/common.js
@@ -1,5 +1,4 @@
 
-}
 $(document).ready(function () {
     var baseUrl = "https://milos.oroz.space/autocheckpro/";
 // Function to update href attributes
@@ -8,5 +7,5 @@ function updateHrefAttributes() {
   $(".dynamic-href").attr("href", function () {
     return baseUrl + $(this).data("path");
   });
-
+}
 });
