0230db8f68961dd91469306aafdf9844ad9e41a2
Milos Oroz <milos.oroz@gmail.com>
2023-10-17 21:08:51 +0200

    add more resources, fix translations

--- js/i18n.js
+++ js/i18n.js
@@ -1,16 +1,16 @@
 const lngs = {
-  en: { nativeName: 'English' },
-  de: { nativeName: 'Deutsch' }
+  en: { nativeName: "English" },
+  de: { nativeName: "Deutsch" },
 };
 
 const rerender = () => {
   // start localizing, details:
   // https://github.com/i18next/jquery-i18next#usage-of-selector-function
-  $('body').localize();
+  $("body").localize();
 
-  $('title').text($.t('head.title'))
-  $('meta[name=description]').attr('content', $.t('head.description'))
-}
+  $("title").text($.t("head.title"));
+  $("meta[name=description]").attr("content", $.t("head.description"));
+};
 
 $(function () {
   // use plugins and options as needed, for options, detail see
@@ -22,104 +22,119 @@ $(function () {
     .use(i18nextHttpBackend)
     // init i18next
     // for all options read: https://www.i18next.com/overview/configuration-options
-    .init({
-      debug: false,
-      fallbackLng: 'en',
-      load: 'languageOnly',
-      backend: {
-        loadPath: '/locales/{{lng}}/{{ns}}.json'
-      }
-      // resources: {
-      //   en: {
-      //     translation: {
-      //       head: {
-      //         title: 'My Awesome Landing-Page',
-      //         description: 'The description of this awesome landing page.'
-      //       },
-      //       intro: {
-      //         title: 'Landing Page',
-      //         subTitle: 'Some subtitle'
-      //       },
-      //       footer: {
-      //         counter_one: 'Changed language just once',
-      //         counter_other: 'Changed language already {{count}} times',
-      //         date: 'It\'s {{date, LLLL}}'
-      //       }
-      //     }
-      //   },
-      //   de: {
-      //     translation: {
-      //       head: {
-      //         title: 'Meine grossartige Webseite',
-      //         description: 'Die Beschreibung dieser grossartigen Webseite.'
-      //       },
-      //       intro: {
-      //         title: 'Webseite',
-      //         subTitle: 'Ein Untertitel'
-      //       },
-      //       footer: {
-      //         counter_one: 'Die Sprache wurde erst ein mal gewechselt',
-      //         counter_other: 'Die Sprache wurde {{count}} mal gewechselt',
-      //         date: 'Es ist {{date, LLLL}}'
-      //       }
-      //     }
-      //   }
-      // }
-    }, (err, t) => {
-      if (err) return console.error(err);
+    .init(
+      {
+        debug: false,
+        fallbackLng: "en",
+        load: "languageOnly",
+        backend: {
+          loadPath: "/locales/{{lng}}/{{ns}}.json",
+        },
+        // resources: {
+        //   en: {
+        //     translation: {
+        //       head: {
+        //         title: 'My Awesome Landing-Page',
+        //         description: 'The description of this awesome landing page.'
+        //       },
+        //       intro: {
+        //         title: 'Landing Page',
+        //         subTitle: 'Some subtitle'
+        //       },
+        //       footer: {
+        //         counter_one: 'Changed language just once',
+        //         counter_other: 'Changed language already {{count}} times',
+        //         date: 'It\'s {{date, LLLL}}'
+        //       }
+        //     }
+        //   },
+        //   de: {
+        //     translation: {
+        //       head: {
+        //         title: 'Meine grossartige Webseite',
+        //         description: 'Die Beschreibung dieser grossartigen Webseite.'
+        //       },
+        //       intro: {
+        //         title: 'Webseite',
+        //         subTitle: 'Ein Untertitel'
+        //       },
+        //       footer: {
+        //         counter_one: 'Die Sprache wurde erst ein mal gewechselt',
+        //         counter_other: 'Die Sprache wurde {{count}} mal gewechselt',
+        //         date: 'Es ist {{date, LLLL}}'
+        //       }
+        //     }
+        //   }
+        // }
+      },
+      (err, t) => {
+        if (err) return console.error(err);
 
-      // define the formatter function
-      // i18next.services.formatter.add('LLLL', (value, lng, options) => {
-      //   return moment(value).locale(lng).format('LLLL');
-      // });
+        // define the formatter function
+        // i18next.services.formatter.add('LLLL', (value, lng, options) => {
+        //   return moment(value).locale(lng).format('LLLL');
+        // });
 
-      // for options see
-      // https://github.com/i18next/jquery-i18next#initialize-the-plugin
-      jqueryI18next.init(i18next, $, { useOptionsAttr: true });
+        // for options see
+        // https://github.com/i18next/jquery-i18next#initialize-the-plugin
+        jqueryI18next.init(i18next, $, { useOptionsAttr: true });
 
-      // fill language switcher
-      // Object.keys(lngs).map((lng) => {
-      //   const opt = new Option(lngs[lng].nativeName, lng);
-      //   if (lng === i  ) {
-      //     opt.setAttribute("selected", "selected");
-      //   }
-      //   $('#languageSwitcher').append(opt);
-      // });
-      // let languageChangedCounter = 0;
+        // fill language switcher
+        // Object.keys(lngs).map((lng) => {
+        //   const opt = new Option(lngs[lng].nativeName, lng);
+        //   if (lng === i  ) {
+        //     opt.setAttribute("selected", "selected");
+        //   }
+        //   $('#languageSwitcher').append(opt);
+        // });
+        // let languageChangedCounter = 0;
 
-      // $('#languageSwitcher').change((a, b, c) => {
-      //   const chosenLng = $(this).find("option:selected").attr('value');
-      //   i18next.changeLanguage(chosenLng, () => {
-      //     rerender();
+        // $('#languageSwitcher').change((a, b, c) => {
+        //   const chosenLng = $(this).find("option:selected").attr('value');
+        //   i18next.changeLanguage(chosenLng, () => {
+        //     rerender();
 
-      //     // language changed message
-      //     languageChangedCounter++;
-      //     $('#languageChangedNotification').localize({ count: languageChangedCounter })
-      //     if (languageChangedCounter === 1) {
-      //       $('#languageChangedNotification').show();
-      //     }
-      //   });
-      // });
+        //     // language changed message
+        //     languageChangedCounter++;
+        //     $('#languageChangedNotification').localize({ count: languageChangedCounter })
+        //     if (languageChangedCounter === 1) {
+        //       $('#languageChangedNotification').show();
+        //     }
+        //   });
+        // });
 
-      // Handle language switch on click
-      var languageSwitch = $('#languageSwitch');
+        // Handle language switch on click
+        // var languageSwitch = $('#languageSwitch');
 
-      languageSwitch.on('click', function () {
-        // Determine the current language
-        var currentLanguage = i18next.resolvedLanguage.split('-')[0];
+        // languageSwitch.on('click', function () {
+        //   // Determine the current language
+        //   var currentLanguage = i18next.resolvedLanguage.split('-')[0];
 
-        // Toggle between English (en) and German (de)
-        var newLanguage = currentLanguage === 'en' ? 'de' : 'en';
+        //   // Toggle between English (en) and German (de)
+        //   var newLanguage = currentLanguage === 'en' ? 'de' : 'en';
 
-        console.log('<<<', currentLanguage, newLanguage, languageSwitch.text())
-        // Update the language switcher
-        i18next.changeLanguage(newLanguage, () => {
-          languageSwitch.text(currentLanguage)
+        //   console.log('<<<', currentLanguage, newLanguage, languageSwitch.text())
+        //   // Update the language switcher
+        //   i18next.changeLanguage(newLanguage, () => {
+        //     languageSwitch.text(currentLanguage)
 
-          rerender();
+        //     rerender();
+        //   });
+        // });
+        // Handle language switch on click
+        $(".language-flag").on("click", function () {
+          // Get the language from the data-lang attribute
+          var newLanguage = $(this).data("lang");
+
+          // Update the language switcher
+          i18next.changeLanguage(newLanguage, () => {
+            // Optionally, you can update the language text if needed
+            // languageSwitch.text(newLanguage);
+
+            rerender();
+          });
         });
-      });
-
-      rerender();
-    });
+        rerender();
+      }
+    );
 });
